48b0f0ee9c7c43e7884ee971eadbaeb8c57a8935
jvanheesch <joris.vanheesch@vdab.be>
2022-03-30 13:13:25 +0200

    fix ExampleIT using testcontainers

--- application/src/test/java/com/github/jvanheesch/ExampleIT.java
+++ application/src/test/java/com/github/jvanheesch/ExampleIT.java
@@ -2,14 +2,28 @@ package com.github.jvanheesch;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.web.client.RestTemplate;
+import org.testcontainers.containers.GenericContainer;
+import org.testcontainers.junit.jupiter.Container;
+import org.testcontainers.junit.jupiter.Testcontainers;
 
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+
+@Testcontainers
 class ExampleIT {
+
+    @Container
+    private static final GenericContainer<?> STUBS_CONTAINER = new GenericContainer<>("docker.io/library/stubs:1.0-SNAPSHOT")
+            .withExposedPorts(8080);
+
     @Test
     void example() {
         RestTemplate restTemplate = new RestTemplate();
 
-        String body = restTemplate.getForEntity("http://localhost:8080/hello", String.class).getBody();
+        String url = String.format("http://%s:%s/" + "hello", STUBS_CONTAINER.getHost(), STUBS_CONTAINER.getMappedPort(8080));
 
-        System.out.println(body);
+        String body = restTemplate.getForEntity(url, String.class).getBody();
+
+        assertThat(body)
+                .isEqualTo("world");
     }
 }
